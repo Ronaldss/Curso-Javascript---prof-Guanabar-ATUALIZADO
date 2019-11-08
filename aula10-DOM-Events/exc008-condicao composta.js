758bb397c3422772193b5bc991e1e8b8513ed2b4
RonaldsS <ronald_ss@outlook.com>
2019-11-08 09:48:32 -0300

    DOM e Events

---
+++ aula10-DOM-Events/exc008-condicao composta.js
@@ -0,0 +1,11 @@
+// Estrutura IF - Condição composta
+
+
+var pais = 'EUA'
+console.log(`Vivendo no ${pais}`)
+if ( pais != "Brasil" ){
+    console.log('Voce é estrangeiro.')
+}else{
+    console.log('Voce é brasileiro!')
+}
+
---
+++ aula10-DOM-Events/exc008-condicao simples.js
@@ -0,0 +1,9 @@
+// Extrutura IF - Condição simples
+
+
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+if ( vel  > 80 ) {
+    console.log('Veículo multado - ecesso de velocidade!')
+}
+console.log('Sempre use o cinto de segurança!')
