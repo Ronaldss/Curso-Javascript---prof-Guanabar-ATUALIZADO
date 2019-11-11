0733e1a2af1b791a70e2def049041284d15854d8
RonaldsS <ronald_ss@outlook.com>
2019-11-11 09:33:05 -0300

    tabuada

--- aula 14 - exercicio/ex016/script.js
+++ aula 14 - exercicio/ex016/script.js
@@ -5,25 +5,32 @@ function contar(){
     var passo = Number(document.getElementById('passo').value)
 
     // Validações
-    if(v1 > v2){
-        window.alert("Inicio não pode ser maior que fim!")
-    }else if(v1<=0){
+    if(v1<=0){
         window.alert("O campo 'Inicio' não pode ter valor \n menor que zero!")
+    }else if(v1 >= v2 && passo > 0){
+        calcularN()
     }else if(passo == 0){
         passo = 1
         window.alert("Estamos considerando variável passo\n com valor igual a 1 (um).")
-        calcular()
+        if(v1>=v2){
+            calcularN()
+        }else{calcularP()}
     }else{
-        calcular()
+        calcularP()
     }
     casa.innerHTML += ` \u{1F600}`
 
     // Contando
-    function calcular(){
+    function calcularP(){
         for(v1; v1 <= v2 ; v1 = v1+passo ){
             casa.innerHTML += `${v1} \u{1F449}` // Buscar no Google por 'unicode emoji list   U+1F600 = \u{1F600}'
         } 
-    }   
+    }  
+    function calcularN(){
+        for(v1; v1 >= v2 ; v1-=passo ){
+            casa.innerHTML += `${v1} \u{1F449}` // Buscar no Google por 'unicode emoji list   U+1F600 = \u{1F600}'
+        } 
+    } 
 }
 
 
