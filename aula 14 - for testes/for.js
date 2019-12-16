6ea1fdcfc69ba603cec373ba53c8626ccf910b48
unknown <ronaldcontact2019@gmail.com>
2019-12-16 10:58:12 -0300

    testando while

--- aula 14 - for testes/for.js
+++ aula 14 - for testes/for.js
@@ -1,12 +1,12 @@
-/*
+
 var i = 0
 while(i<=3){
     console.log(`${i} Ronald`)
     i++
 }
-*/
-console.log('Vai começar!')
-for(i=0;i<=3;i++){
-    console.log(`${i}`)
-}
-console.log('Fim!')
+
+// console.log('Vai começar!')
+// for(i=0;i<=3;i++){
+//     console.log(`${i}`)
+// }
+// console.log('Fim!')
